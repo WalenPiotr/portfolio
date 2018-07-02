7c4b14d7f7d36e7b6d10c8a91758a521e59d7e0a
Piotr Walen <walen.piotr@gmail.com>
2018-07-02 21:16:15 +0200

    Add project panes

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { theme } from '@constants/theme';
+import theme from '@constants/theme';
 
 const Box = styled.div`
     display: flex;
@@ -14,9 +14,33 @@ const Title = styled.span`
     font-size: 35px;
 `;
 
+const PaneGroup = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2vh;
+`;
+
+const Pane = styled.span`
+    width: 30vw;
+    height: 20vh;
+    margin: 2vh;
+    border-bottom: 2px dashed ${theme.fontPrimaryColor};
+    border-top: 2px dashed ${theme.fontPrimaryColor};
+`;
+
 const Projects = () => (
-    <Box theme={theme}>
+    <Box>
         <Title>Projects</Title>
+        <PaneGroup>
+            <Pane />
+            <Pane />
+            <Pane />
+            <Pane />
+            <Pane />
+            <Pane />
+        </PaneGroup>
     </Box>
 );
 
