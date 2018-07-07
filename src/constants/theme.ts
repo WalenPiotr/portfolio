2c6cb9f2b0bea7fbf3f19923e22f6f0d9c8ff338
Piotr Walen <walen.piotr@gmail.com>
2018-07-07 07:29:37 -0500

    Add navbar background theme

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -2,7 +2,8 @@ const theme = {
     fontPrimaryColor: 'rgb(190, 190, 190)',
     fontHighlightColor: 'rgb(220, 220, 220)',
     boxShadowColor: 'rgb(150,150,150)',
-    backgroundColor: 'rgb(48, 66, 112)',
+    backgroundColor: 'rgb(38, 56, 102)',
+    navbarColor: 'rgb(28, 46, 92)',
 };
 
 export default theme;
