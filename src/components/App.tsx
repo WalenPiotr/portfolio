1488b679734e621d1d1edf31b7de07b032387e00
Piotr Walen <walen.piotr@gmail.com>
2018-07-07 07:29:58 -0500

    Move constant out of class

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,10 +7,72 @@ import Skills from '@pages/Skills';
 import IView from '@typings/IView';
 import * as React from 'react';
 
+const projects = [
+    {
+        name: 'Yet Another To-Do List',
+        technologies: [
+            'TypeScript',
+            'React',
+            'Redux',
+            'styled-components',
+            'NodeJS',
+            'Express',
+            'PostgreSQL',
+        ],
+        description: 'Todo list app created with TypeScript.',
+        links: new Map([
+            ['Source code', 'https://github.com/WalenPiotr/YetAnotherToDoApp'],
+            ['Demo page', 'https://todos-typescript.herokuapp.com/'],
+        ]),
+    },
+    {
+        name: 'Scrabble Detector',
+        technologies: [
+            'Python',
+            'Keras',
+            'Tensorflow',
+            'OpenCV',
+            'Android (Java)',
+            'JUnit',
+        ],
+        description:
+            'Automatic point counting system for Scrabble game. Application detects game board and tiles with letter, using OpenCV image processing library, then classifies each tile with two convolutional neural networks built in Keras, and finally counts points for each player turn.',
+        links: new Map([
+            ['Source code', 'https://github.com/WalenPiotr/Scrabble-Detector'],
+        ]),
+    },
+    {
+        name: 'Rate Car',
+        technologies: ['NodeJS', 'Express', 'MongoDB', 'EJS Templates'],
+        description:
+            'Car review application with server side rendering, user authentication and authorization.',
+        links: new Map([
+            ['Source code', 'https://github.com/WalenPiotr/Rate-Car'],
+            ['Demo page', 'https://rate-car.herokuapp.com/'],
+        ]),
+    },
+];
+
+const views = [
+    { name: 'Home', component: <Home /> },
+    {
+        name: 'Skills',
+        component: <Skills />,
+    },
+    {
+        name: 'Projects',
+        component: <Projects projects={projects} />,
+    },
+    {
+        name: 'Credits',
+        component: <Credits />,
+    },
+];
+
 export interface AppProps {
     setCurrentPage: (page: number) => void;
+    currentPage: number;
 }
-
 class App extends React.Component<AppProps, any> {
     constructor(props: AppProps) {
         super(props);
@@ -31,7 +93,9 @@ class App extends React.Component<AppProps, any> {
 
     handleScroll = (event: Event) => {
         const currentPage = Math.round(window.pageYOffset / window.innerHeight);
-        this.props.setCurrentPage(currentPage);
+        if (currentPage !== this.props.currentPage) {
+            this.props.setCurrentPage(currentPage);
+        }
     };
 
     boxes: Map<IView, HTMLDivElement>;
@@ -49,73 +113,6 @@ class App extends React.Component<AppProps, any> {
     };
 
     render() {
-        const projects = [
-            {
-                name: 'Yet Another To-Do List',
-                technologies: [
-                    'TypeScript',
-                    'React',
-                    'Redux',
-                    'styled-components',
-                    'NodeJS',
-                    'Express',
-                    'PostgreSQL',
-                ],
-                description: 'Todo list app created with TypeScript.',
-                links: new Map([
-                    [
-                        'Source code',
-                        'https://github.com/WalenPiotr/YetAnotherToDoApp',
-                    ],
-                    ['Demo page', 'https://todos-typescript.herokuapp.com/'],
-                ]),
-            },
-            {
-                name: 'Scrabble Detector',
-                technologies: [
-                    'Python',
-                    'Keras',
-                    'Tensorflow',
-                    'OpenCV',
-                    'Android (Java)',
-                    'JUnit',
-                ],
-                description:
-                    'Automatic point counting system for Scrabble game. Application detects game board and tiles with letter, using OpenCV image processing library, then classifies each tile with two convolutional neural networks built in Keras, and finally counts points for each player turn.',
-                links: new Map([
-                    [
-                        'Source code',
-                        'https://github.com/WalenPiotr/Scrabble-Detector',
-                    ],
-                ]),
-            },
-            {
-                name: 'Rate Car',
-                technologies: ['NodeJS', 'Express', 'MongoDB', 'EJS Templates'],
-                description:
-                    'Car review application with server side rendering, user authentication and authorization.',
-                links: new Map([
-                    ['Source code', 'https://github.com/WalenPiotr/Rate-Car'],
-                    ['Demo page', 'https://rate-car.herokuapp.com/'],
-                ]),
-            },
-        ];
-        const views = [
-            { name: 'Home', component: <Home /> },
-            {
-                name: 'Skills',
-                component: <Skills />,
-            },
-            {
-                name: 'Projects',
-                component: <Projects projects={projects} />,
-            },
-            {
-                name: 'Credits',
-                component: <Credits />,
-            },
-        ];
-
         return (
             <div>
                 <Navbar views={views} createHandler={this.createHandler} />
