81ab1c0427574706d5d5c2efd43ae18cda5cb443
Piotr Walen <walen.piotr@gmail.com>
2018-07-11 08:40:59 -0500

    Change colors

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -2,8 +2,8 @@ const theme = {
     fontPrimaryColor: 'rgb(220, 220, 220)',
     fontHighlightColor: 'rgb(255, 255, 255)',
     boxShadowColor: 'rgb(150,150,150)',
-    backgroundColor: 'rgb(20, 20, 80)',
-    navbarColor: 'rgb(20, 20, 80)',
+    backgroundColor: 'rgba(0, 0, 0, 0.0)',
+    navbarColor: 'rgba(20, 20, 20)',
     paralaxColor: 'rgb(30, 30, 30)',
 };
 
